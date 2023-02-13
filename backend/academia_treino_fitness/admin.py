7937c021afb0c12fb8f4560a2551a8a097c49b44
Josué Luiz Barbosa e Silva <104951932+josuelb@users.noreply.github.com>
2023-02-13 10:44:16 -0300

    Update admin.py

--- backend/academia_treino_fitness/admin.py
+++ backend/academia_treino_fitness/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+@admin.register(Cliente)
+class Cliente(admin.ModelAdmin):
+    list_display = '__all__'
+    list_display_links = (
+        'id',
+        'nome',
+        'user',
+        'cpf',
+        'rg',
+        'email'
+    )
+    search_fields = '__all__'
+
+
+@admin.register(Funcionario)
+class Funcionario(admin.ModelAdmin):
+    list_display = '__all__'
+    list_display_links = (
+        'id',
+        'nome',
+        'user',
+        'cpf',
+        'rg',
+        'email'
+    )
+    search_fields = '__all__'
+
+
+@admin.register(Cargo)
+class Cargo(admin.ModelAdmin):
+    list_display = '__all__'
+    list_display_links = '__all__'
+    search_fields = '__all__'
+
+
+@admin.register(Plano)
+class Plano(admin.ModelAdmin):
+    list_display = '__all__'
+    list_display_links = '__all__'
+    search_fields = '__all__'
