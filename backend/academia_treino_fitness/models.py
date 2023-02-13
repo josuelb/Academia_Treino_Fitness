d7be3c52d0c7cc3e8d9bb8579f04a961b5709c1f
Josué Luiz Barbosa e Silva <104951932+josuelb@users.noreply.github.com>
2023-02-13 10:22:20 -0300

    Criação do models.py

--- backend/academia_treino_fitness/models.py
+++ backend/academia_treino_fitness/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+# Modelos de bancos de dados
+def Photo(instance, filename):
+    return f'{instance.Pessoa}-{filename}'
+
+
+class Pessoa(models.Model):
+    foto = models.ImageField(upload_to=Photo, blank=True, null=False)
+    nome = models.CharField(max_length=100, null=False)
+    sobrenome = models.CharField(max_length=100, null=False)
+    data_nasc = models.DateField()
+    cpf = models.CharField(max_length=11, null=False, unique=True)
+    rg = models.IntegerField(null=False, unique=True)
+
+    class Meta:
+        abstract = True
+
+
+class Cargo(models.Model):
+    nome = models.CharField(max_length=50)
+    remuneracao = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.nome}'
+
+
+class Plano(models.Model):
+    plano = models.CharField(max_length=50)
+    valor = models.IntegerField(max_length=10)
+
+    def __str__(self):
+        return f'{self.plano}'
+
+
+class Cliente(Pessoa):
+    user = models.CharField(max_length=50, null=False, unique=True)
+    email = models.EmailField(null=False)
+    password = models.CharField(null=False)
+    situacao = models.CharField(max_length=7, null=False)
+    plano = models.ForeignKey(Plano, on_delete=models.CASCADE, default='Mensal')
+    objetivo = models.CharField(max_length=100)
+
+
+class Funcionario(Pessoa):
+    user = models.CharField(max_length=50, null=False, unique=True)
+    email = models.EmailField(null=False)
+    password = models.CharField(null=False)
+    cargo = models.ForeignKey(Cargo, on_delete=models.CASCADE, default=None)
+
